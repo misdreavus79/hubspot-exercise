d81c56716fab7eeec8ca45c00a4f19b85c24eef8
Eli Moreta-Feliz <eliezer.moreta-feliz@macys.com>
2016-07-13 17:51:57 -0400

    added listings partial

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,12 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	cssnano = require('gulp-cssnano'),
 	concat = require('gulp-concat'),
-	ejs = require('gulp-ejs');
+	ejs = require('gulp-ejs'),
+	browserSync = require("browser-sync").create(),
+	gutil = require('gulp-util'),
+	json = require('./src/js/data/data.json'); //simply reading the data in, unless there's a requirement to load it asynchronously
 
- 
-
-gulp.task('clean:css', function(){ //I use Grunt as my task builder,
+gulp.task('clean:css', function(){ //I use Grunt as my task runner,
 
 });
 
@@ -19,7 +20,14 @@ gulp.task('clean:js', function(){ //please forgive me if some of these tasks are
 
 });
 
-gulp.task('css', ['clean:css'], function() { //I'm assuming this task (css)
+gulp.task('sass', function() {
+    return gulp.src("./src/scss/*.scss")
+		    .pipe(sass())
+		    .pipe(gulp.dest("./build/css"))
+		    .pipe(browserSync.stream());
+});
+
+gulp.task('css', ['clean:css'], function() { 
 
 });
 
@@ -28,20 +36,34 @@ gulp.task('js', ['clean:js'], function() {
 });
 
 gulp.task('ejs', function() {
-	gulp.src("./src/views/pages/*.ejs")
-	.pipe(ejs()) //pass the ext: html option if you want to output html
-	.pipe(gulp.dest("./build"));
+	return gulp.src("./src/views/pages/*.ejs")
+			.pipe(ejs(
+			{
+				data: json
+			}, 
+			{
+				ext: '.html' //compile into html 
+			}
+			)).on('error', gutil.log)
+			.pipe(gulp.dest("./build"));
 });
 
 gulp.task('html', function() {
-
+	//not really using this since 
+	//I'm compiling to html from the get go
 });
 
 gulp.task('serve', function() {
-
+	browserSync.init({
+        server: "./build"
+    });
 });
 
 // The default task (called when we run `gulp` from cli)
-gulp.task('default', ['ejs', 'js', 'sass'], function() { //refers to this task (sass)?
+gulp.task('default', ['ejs', 'js', 'sass', 'serve'], function() { 
 
+	//watch files and perform propper actions
+	gulp.watch("./src/scss/*.scss", ['sass']);
+	gulp.watch("./src/views/**/*.ejs", ['ejs']);
+    gulp.watch("./build/*.html").on('change', browserSync.reload);
 });
